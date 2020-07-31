91bf49bb98cecfd043c31a305d692591df2c1714
JenKinY <youzhengcai@gmail.com>
2020-07-31 14:24:35 +0800

    1.修改日志等级

--- run.py
+++ run.py
@@ -3,21 +3,21 @@ import config_dev,config_test,config_prod
 from os import path
 
 if __name__ == '__main__':
-    # logging.basicConfig(level=logging.NOTSET)
+    logging.basicConfig(level=logging.WARNING)
     try:
         DEPLOYMENT_ENV = sys.argv[1]
 
         if DEPLOYMENT_ENV == 'dev':
             # 开发环境
-            logging.info(f'部署成功！当前环境:{DEPLOYMENT_ENV}')
+            logging.warning(f'部署成功！当前环境:{DEPLOYMENT_ENV}')
             nonebot.init(config_dev)
         elif DEPLOYMENT_ENV == 'test':
             # 测试环境
-            logging.info(f'部署成功！当前环境:{DEPLOYMENT_ENV}')
+            logging.warning(f'部署成功！当前环境:{DEPLOYMENT_ENV}')
             nonebot.init(config_test)
         elif DEPLOYMENT_ENV == 'prod':
             # 生产环境
-            logging.info(f'部署成功！当前环境:{DEPLOYMENT_ENV}')
+            logging.warning(f'部署成功！当前环境:{DEPLOYMENT_ENV}')
             nonebot.init(config_prod)
         else:
             logging.warning('参数错误！请输入正确的运行参数:\ndev:开发环境\nprod:生产环境\ntest:测试环境')
