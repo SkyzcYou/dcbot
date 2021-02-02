9e36ef854f107a580ef04db580092d7161ca3343
Skyzc <youzhengcai@gmail.com>
2021-02-02 22:52:33 +0800

    Update config_test.py

--- config_test.py
+++ config_test.py
@@ -5,5 +5,5 @@ import re
 
 HOST = '127.0.0.1'                              # 运行地址
 PORT = 8080                                     # 运行端口
-SUPERUSERS = {1404725663}                       # 管理员QQ
+SUPERUSERS = {149234xxxxx}                       # 管理员QQ
 COMMAND_START = ['', re.compile(r'[/!]+')]      # 配置命令的起始字符
